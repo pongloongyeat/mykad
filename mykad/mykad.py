17ff3c2cfc9794001a3ccebf50297d723c6987fc
Pong Loong Yeat <pongloongyeat@gmail.com>
2021-05-24 05:52:33 +0800

    Birthyear is apparently not a word

--- mykad/mykad.py
+++ mykad/mykad.py
@@ -53,9 +53,9 @@ class MyKad:
 
         # If MyKad is valid we should extract the information out of it
         # YYMMDD-PB-###G
-        self.birthyear_num = self.mykad_num[0:2]
-        self.birthmonth_num = self.mykad_num[2:4]
-        self.birthday_num = self.mykad_num[4:6]
+        self.birth_year_num = self.mykad_num[0:2]
+        self.birth_month_num = self.mykad_num[2:4]
+        self.birth_day_num = self.mykad_num[4:6]
         self.birthplace_num = self.mykad_num[6:8]
         self.nrd_num = self.mykad_num[8:11]
         self.gender_num = self.mykad_num[-1]
@@ -76,23 +76,23 @@ class MyKad:
         """
         return f'{self.birthyear_num}{self.birthmonth_num}{self.birthday_num}-{self.birthplace_num}-{self.nrd_num}{self.gender_num}'
 
-    def get_birthyear(self):
-        """Returns the birthyear of the MyKad holder.
+    def get_birth_year(self):
+        """Returns the birth year of the MyKad holder.
 
-        :return: The birthyear in YYYY format
+        :return: The birth year in YYYY format
         :rtype: str
         """
 
         # MyKads started being issued in the year 1949
-        if int(self.birthyear_num) >= 49:
-            return f'19{self.birthyear_num}'
+        if int(self.birth_year_num) >= 49:
+            return f'19{self.birth_year_num}'
 
-        return f'20{self.birthyear_num}'
+        return f'20{self.birth_year_num}'
 
-    def get_birthmonth(self):
-        """Returns the birthmonth of the MyKad holder.
+    def get_birth_month(self):
+        """Returns the birth month of the MyKad holder.
 
-        :return A birthmonth in English
+        :return A birth month in English
         :rtype str
         """
 
@@ -111,4 +111,4 @@ class MyKad:
             '12': 'December',
         }
 
-        return month_dict[self.birthmonth_num]
+        return month_dict[self.birth_month_num]
