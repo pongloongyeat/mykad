743f0db3f415780a1bf8da8aaf46b4f9e515c03e
pongloongyeat <pongloongyeat@gmail.com>
2021-06-12 03:49:20 +0800

    Use variable names that make more sense

--- src/mykad/mykad.py
+++ src/mykad/mykad.py
@@ -15,12 +15,12 @@ class MyKad:
 
         # If MyKad is valid we should extract the information out of it
         # YYMMDD-PB-###G
-        self.birth_year_num = self.mykad_num[0:2]
-        self.birth_month_num = self.mykad_num[2:4]
-        self.birth_day_num = self.mykad_num[4:6]
-        self.birthplace_num = self.mykad_num[6:8]
-        self.nrd_num = self.mykad_num[8:11]
-        self.gender_num = self.mykad_num[-1]
+        self.birth_year = self.mykad_num[0:2]
+        self.birth_month = self.mykad_num[2:4]
+        self.birth_day = self.mykad_num[4:6]
+        self.birthplace_code = self.mykad_num[6:8]
+        self.special_nrd_num = self.mykad_num[8:11]
+        self.gender_code = self.mykad_num[-1]
 
     def get_unformatted(self):
         """Returns the unformatted MyKad string (i.e. just numbers, without '-')
@@ -36,20 +36,20 @@ class MyKad:
         :return: The formatted MyKad number
         :rtype: str
         """
-        return f'{self.birth_year_num}{self.birth_month_num}{self.birth_day_num}-{self.birthplace_num}-{self.nrd_num}{self.gender_num}'
+        return f'{self.birth_year}{self.birth_month}{self.birth_day}-{self.birthplace_code}-{self.special_nrd_num}{self.gender_code}'
 
     def get_birth_year(self):
         """Returns the birth year of the MyKad holder.
 
-        :return: The birth year in YYYY format
+        :return: The birth year in YY format.
         :rtype: str
         """
 
         # MyKads started being issued in the year 1949
-        if int(self.birth_year_num) >= 49:
-            return f'19{self.birth_year_num}'
+        if int(self.birth_year) >= 49:
+            return f'19{self.birth_year}'
 
-        return f'20{self.birth_year_num}'
+        return f'20{self.birth_year}'
 
     def get_birth_month(self):
         """Returns the birth month of the MyKad holder.
@@ -73,13 +73,13 @@ class MyKad:
             '12': 'December',
         }
 
-        return month_dict[self.birth_month_num]
+        return month_dict[self.birth_month]
 
     def is_male(self):
-        return int(self.gender_num) % 2 != 0
+        return int(self.gender_code) % 2 != 0
 
     def is_female(self):
-        return int(self.gender_num) % 2 == 0
+        return int(self.gender_code) % 2 == 0
 
     def get_gender(self):
         """Returns the gender of the MyKad holder.
