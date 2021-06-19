8a3d5a52f008f49299fbeb70d97edde0df6dd200
pongloongyeat <pongloongyeat@gmail.com>
2021-06-19 14:37:48 +0800

    Only test methods that could cause issues
    Add birthplace and gender check

--- tests/test_mykad.py
+++ tests/test_mykad.py
@@ -10,9 +10,8 @@ def test_mykad_class():
     random_mykad_num, random_date, random_state, random_gender = generate_random_valid_mykad()
     mykad = MyKad(random_mykad_num)
 
-    assert mykad.get_birth_year() == random_date.strftime('%y')
     assert mykad.get_pretty_birth_year() == random_date.strftime('%Y')
-    assert mykad.get_birth_month() == random_date.strftime('%m')
     assert mykad.get_pretty_birth_month() == random_date.strftime('%B')
-    assert mykad.get_birth_day() == random_date.strftime('%d')
     assert mykad.get_pretty_birth_day() == random_date.strftime('%A')
+    assert mykad.get_birthplace() == random_state
+    assert mykad.get_gender() == random_gender
