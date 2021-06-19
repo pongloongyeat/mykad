2d15c7c6ed6db50b2c9eb889d7ae4d281ce3d1e3
pongloongyeat <pongloongyeat@gmail.com>
2021-06-19 14:15:57 +0800

    Use the whole MyKad class as a test case

--- tests/test_mykad.py
+++ tests/test_mykad.py
@@ -1,30 +1,18 @@
-import random
-import time
-from datetime import datetime, timedelta
+import pytest
 from src.mykad.mykad import MyKad
 from .utils import generate_random_valid_mykad
 
 
-class TestMyKad:
-    """Generates a random valid MyKad number
+@pytest.mark.repeat(1000)
+def test_mykad_class():
+    """Generates a random valid MyKad instance
     and checks if it gets it right."""
     random_mykad_num, random_date, random_state, random_gender = generate_random_valid_mykad()
     mykad = MyKad(random_mykad_num)
 
-    def test_mykad_get_birth_year(self):
-        assert self.mykad.get_birth_year() == self.random_date.strftime('%y')
-
-    def test_mykad_get_pretty_birth_year(self):
-        assert self.mykad.get_pretty_birth_year() == self.random_date.strftime('%Y')
-
-    def test_mykad_get_birth_month(self):
-        assert self.mykad.get_birth_month() == self.random_date.strftime('%m')
-
-    def test_mykad_get_pretty_birth_month(self):
-        assert self.mykad.get_pretty_birth_month() == self.random_date.strftime('%B')
-
-    def test_mykad_get_birth_day(self):
-        assert self.mykad.get_birth_day() == self.random_date.strftime('%d')
-
-    def test_mykad_get_pretty_birth_day(self):
-        assert self.mykad.get_pretty_birth_day() == self.random_date.strftime('%A')
+    assert mykad.get_birth_year() == random_date.strftime('%y')
+    assert mykad.get_pretty_birth_year() == random_date.strftime('%Y')
+    assert mykad.get_birth_month() == random_date.strftime('%m')
+    assert mykad.get_pretty_birth_month() == random_date.strftime('%B')
+    assert mykad.get_birth_day() == random_date.strftime('%d')
+    assert mykad.get_pretty_birth_day() == random_date.strftime('%A')
