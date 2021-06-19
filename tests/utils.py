d985926feeca47d69c301623a54a62bcb5edc27b
pongloongyeat <pongloongyeat@gmail.com>
2021-06-19 14:15:29 +0800

    Move random valid MyKad geneneration to utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,26 @@
 import random
+import time
+from datetime import datetime, timedelta
+
+
+state_dict = {
+    'Johor': [1, 21, 22, 23, 24],
+    'Kedah': [2, 25, 26, 27],
+    'Kelantan': [3, 28, 29],
+    'Malacca': [4, 30],
+    'Negeri Sembilan': [5, 31, 59],
+    'Pahang': [6, 32, 33],
+    'Penang': [7, 34, 35],
+    'Perak': [8, 36, 37, 38, 39],
+    'Perlis': [9, 40],
+    'Selangor': [10, 41, 42, 43, 44],
+    'Terengganu': [11, 45, 46],
+    'Sabah': [12, 47, 48, 49],
+    'Sarawak': [13, 50, 51, 52 ,53],
+    'Federal Territory of Kuala Lumpur': [14, 54, 55, 56, 57],
+    'Federal Territory of Labuan': [15, 58],
+    'Federal Territory of Putrajaya': [16, 59],
+}
 
 
 def generate_valid_mykad(date, state, gender):
@@ -20,25 +42,6 @@ def generate_valid_mykad(date, state, gender):
     if date.year < 1949:
         raise ValueError('year_yyyy must be >= 1949')
 
-    state_dict = {
-        'Johor': [1, 21, 22, 23, 24],
-        'Kedah': [2, 25, 26, 27],
-        'Kelantan': [3, 28, 29],
-        'Malacca': [4, 30],
-        'Negeri Sembilan': [5, 31, 59],
-        'Pahang': [6, 32, 33],
-        'Penang': [7, 34, 35],
-        'Perak': [8, 36, 37, 38, 39],
-        'Perlis': [9, 40],
-        'Selangor': [10, 41, 42, 43, 44],
-        'Terengganu': [11, 45, 46],
-        'Sabah': [12, 47, 48, 49],
-        'Sarawak': [13, 50, 51, 52 ,53],
-        'Federal Territory of Kuala Lumpur': [14, 54, 55, 56, 57],
-        'Federal Territory of Labuan': [15, 58],
-        'Federal Territory of Putrajaya': [16, 59],
-    }
-
     for key, val in state_dict.items():
         if key.lower() == state.lower():
             random_birthplace_code = str(val[random.randint(0, len(val) - 1)])
@@ -57,3 +60,25 @@ def generate_valid_mykad(date, state, gender):
             return f'{date.strftime("%y%m%d")}{random_birthplace_code}{random_nrd_num}{random_gender_num}'
 
     raise ValueError(f'could not find state {state}')
+
+def generate_random_valid_mykad():
+    """Generate a random valid MyKad number
+    """
+    # Get a random date from 1949 to now
+    start_date = datetime(1949, 1, 1)
+    end_date = datetime.now()
+
+    difference = (end_date - start_date).days
+
+    random_date = start_date + timedelta(days=random.randint(0, difference))
+
+    # Choose a random state
+    random_state = list(state_dict)[random.randint(0, len(state_dict) - 1)]
+
+    # Flip a coin for gender
+    if random.randint(0, 1) == 0:
+        random_gender = 'Male'
+    else:
+        random_gender = 'Female'
+
+    return generate_valid_mykad(random_date, random_state, random_gender), random_date, random_state, random_gender
