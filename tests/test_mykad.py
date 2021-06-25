ee45a091b5f1f95fe6f5532e5a4997b19871c692
Pong Loong Yeat <pongloongyeat@gmail.com>
2021-06-25 13:31:48 +0000

    Add __str__ method and test formatted MyKad numbers (#10)
    * Add __str__ for nicer prints
    
    * Add test for formatted MyKad
    
    * Print formatted
    
    * Typo

--- tests/test_mykad.py
+++ tests/test_mykad.py
@@ -15,3 +15,19 @@ def test_mykad_class():
     assert mykad.day_of_birth == random_date.day
     assert mykad.birthplace == random_state
     assert mykad.gender == random_gender
+
+@pytest.mark.repeat(1000)
+def test_mykad_class_formatted():
+    """Same as above but with a formatted
+    MyKad number (i.e. YYMMDD-BP-###G aka
+    with the '-' character)."""
+    random_mykad_num, random_date, random_state, random_gender = generate_random_valid_mykad()
+
+    random_mykad_num = f'{random_mykad_num[0:6]}-{random_mykad_num[6:8]}-{random_mykad_num[8:12]}'
+    mykad = MyKad(random_mykad_num)
+
+    assert mykad.birth_year == random_date.year
+    assert mykad.birth_month == random_date.month
+    assert mykad.day_of_birth == random_date.day
+    assert mykad.birthplace == random_state
+    assert mykad.gender == random_gender
